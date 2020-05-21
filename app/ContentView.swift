d65a796e29180e5263d8457071fe6e7090815146
Jianping Gan <jianping@acceptbooking.com>
2020-05-21 09:47:22 +0800

    regenerate a new iOS Single View appliation, update readme.md

---
+++ app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Jianping Gan on 19/5/20.
+//  Copyright © 2020 Jianping Gan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
